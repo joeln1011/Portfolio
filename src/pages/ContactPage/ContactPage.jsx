48aaf213708012f2412d930cc6b7272c7d5ed200
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-08-01 22:54:46 -0400

    Using Emailjs for ontact

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,21 +1,15 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import PhoneIcon from '@mui/icons-material/Phone';
-import TextField from '@mui/material/TextField';
 import EmailIcon from '@mui/icons-material/Email';
-import { useForm } from 'react-hook-form';
-import Grid from '@mui/material/Grid';
+import ContactForm from '../../components/Form/ContactForm';
+
 const ContactPage = () => {
-  const { handleSubmit } = useForm();
-  const submitContactForm = (data) => {
-    console.log('Form submitted:', data);
-    // Here you would typically handle form submission, e.g., sending data to a server
-  };
   return (
     <Box
       sx={{
         display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
         width: '100%',
         minHeight: '100vh',
         padding: '100px 9% 20px',
@@ -35,7 +29,13 @@ const ContactPage = () => {
       }}
     >
       {/* left */}
-      <Box sx={{ maxWidth: { xs: '100%', md: '40%' } }}>
+      <Box
+        sx={{
+          maxWidth: { xs: '100%', md: '30%' },
+          marginRight: { xs: 0, md: '20px' },
+          marginBottom: { xs: '24px', md: 0 },
+        }}
+      >
         <Typography variant="h4">Let's Work Together</Typography>
         <Typography variant="p">
           If you have any questions, want to collaborate on a project, or just
@@ -86,123 +86,34 @@ const ContactPage = () => {
           border: '1px solid #323946',
           backgroundColor: '#323946',
           maxWidth: { xs: '100%', md: '60%' },
+          width: '100%',
         }}
       >
-        <form onSubmit={handleSubmit(submitContactForm)}>
-          <Box
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: { xs: '20px', md: '30px' },
+            gap: '12px',
+          }}
+        >
+          <Typography
+            sx={{ fontSize: { xs: '32px', md: '45px' }, fontWeight: 500 }}
+          >
+            Contact
+          </Typography>
+          <Typography
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: { xs: '20px', md: '30px' },
-              gap: '12px',
+              fontSize: { xs: '32px', md: '45px' },
+              color: '#7cf03d',
+              fontWeight: 500,
             }}
           >
-            <Typography
-              sx={{ fontSize: { xs: '32px', md: '45px' }, fontWeight: 500 }}
-            >
-              Contact
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: { xs: '32px', md: '45px' },
-                color: '#7cf03d',
-                fontWeight: 500,
-              }}
-            >
-              Me!!!
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-              padding: '20px',
-              maxWidth: { xs: '100%', md: '100%' },
-            }}
-          >
-            <Grid
-              container
-              spacing={2}
-              sx={{
-                marginTop: '20px',
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-              }}
-            >
-              <TextField
-                sx={{
-                  backgroundColor: ' #1f242d',
-                  width: { xs: '100%', md: '40%' },
-                }}
-                hiddenLabel
-                type="text"
-                placeholder="Full Name"
-                variant="outlined"
-              />
-              <TextField
-                sx={{
-                  backgroundColor: ' #1f242d',
-                  width: { xs: '100%', md: '40%' },
-                }}
-                hiddenLabel
-                placeholder="Email Address"
-                variant="outlined"
-              />
-              <TextField
-                sx={{
-                  backgroundColor: ' #1f242d',
-                  width: { xs: '100%', md: '40%' },
-                }}
-                hiddenLabel
-                placeholder="Phone Number"
-                variant="outlined"
-              />
-              <TextField
-                sx={{
-                  backgroundColor: ' #1f242d',
-                  width: { xs: '100%', md: '40%' },
-                }}
-                hiddenLabel
-                placeholder="Email Subject"
-                variant="outlined"
-              />
-              <TextField
-                sx={{
-                  backgroundColor: ' #1f242d',
-                  width: { xs: '100%', md: '82%' },
-                }}
-                hiddenLabel
-                placeholder="Your Message..."
-                variant="outlined"
-                multiline
-                rows={10}
-              />
-            </Grid>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                marginTop: { xs: '20px', md: '30px' },
-              }}
-            >
-              <Button
-                type="submit"
-                style={{
-                  padding: '10px 20px',
-                  backgroundColor: '#7cf03d',
-                  color: 'black',
-                  border: 'none',
-                  borderRadius: '50px',
-                  cursor: 'pointer',
-                  fontSize: '16px',
-                }}
-              >
-                Send Message
-              </Button>
-            </Box>
-          </Box>
-        </form>
+            Me!!!
+          </Typography>
+        </Box>
+        <ContactForm />
       </Box>
     </Box>
   );
